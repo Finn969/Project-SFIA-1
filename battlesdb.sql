7381a4a58cb0893626fcf4bd999eb9c8711f5394
Finn969 <61195940+Finn969@users.noreply.github.com>
2020-03-03 16:24:39 +0000

    SQL of database added

---
+++ battlesdb.sql
@@ -0,0 +1,38 @@
+BEGIN;
+
+DROP TABLE IF EXISTS generalstable;
+
+CREATE TABLE generalstable (
+	ID INT(3) NOT NULL AUTO_INCREMENT,
+	firstname VARCHAR(50) DEFAULT = UNKNOWN,
+	lastname VARCHAR(50) NOT NULL,
+	nationality VARCHAR(50) DEFAULT = UNKNOWN,
+	date_of_birth DATE NOT NULL,
+	notes VARCHAR(50)
+	PRIMARY KEY(ID)
+);
+
+DROP TABLE IF EXISTS battlestable;
+
+CREATE TABLE battlestable (
+	ID INT(3) NOT NULL AUTO_INCREMENT,
+	location VARCHAR(50) NOT NULL,
+	startdate DATE,
+	enddate DATE NOT NULL,
+	type VARCHAR(10) NOT NULL
+	PRIMARY KEY(ID)
+);
+
+DROP TABLE IF EXISTS relations;
+
+CREATE TABLE relations (
+	eventID INT(3) NOT NULL AUTO_INCREMENT,
+	generalID INT(3) NOT NULL,
+	battleID INT(3) NOT NULL,
+	PRIMARY KEY(eventID)
+	FOREIGN KEY(generalID) REFERENCES generalstable(ID)
+	FOREIGN KEY(battleID) REFERENCES battlestable(ID)
+);
+
+	
+COMMIT;
