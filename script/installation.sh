2e264297fa9c0d92185f915ba4e027abb3818d54
Finn Macrae <Finn.Macrae@academytrainee.com>
2020-03-16 11:55:05 +0000

    AREUNOBNORIBNEROA%RHOPEIhnoahioaer

--- script/installation.sh
+++ script/installation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source venv/bin/activate
-sudo python3 -m pip install flask
-sudo python3 -m pip install flask_mysqldb
+python3 -m pip install flask
+python3 -m pip install flask_mysqldb
 
 python3 app.py
