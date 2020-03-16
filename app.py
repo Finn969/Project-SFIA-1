3fe1c20c8158e248e453336700cc35474e2aab39
Finn Macrae <Finn.Macrae@academytrainee.com>
2020-03-16 15:22:16 +0000

    simpler webhook method

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ app.config["MYSQL_PASSWORD"] = os.environ['MYSQLPASS']
 app.config["MYSQL_DB"] = os.environ['MYSQLDB']
 mysql= MySQL(app)
 
-#Testing the Jenkins webhook again.
 
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
