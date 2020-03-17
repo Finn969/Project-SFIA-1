5e7d2c9078d32ccdb5d440edd5633e9124cd05f2
Finn Macrae <Finn.Macrae@academytrainee.com>
2020-03-17 11:36:11 +0000

    no.

--- script/installation.sh
+++ script/installation.sh
@@ -6,4 +6,4 @@ python3 -m pip install urllib3
 python3 -m pip install pytest
 python3 -m pip install coverage
 source ~/.bashrc
-python3 . /var/lib/jenkins/workspace/Pipeline1/app.py
+python3 /var/lib/jenkins/workspace/Pipeline1/app.py
